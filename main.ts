c4d4304b2d0693f09343d0c493fde67e848493c3
black-RAM <pplanes311@gmail.com>
2025-07-30 22:27:16 +0300

    leaflet map

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ const app = express();
 const PORT = Number(Deno.env.get('PORT'));
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', indexRouter);
