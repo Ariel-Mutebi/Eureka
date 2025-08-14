9ddeebcd382b8fd823722bf6db8bb57fe568f9d0
black-RAM <pplanes311@gmail.com>
2025-08-14 22:57:23 +0300

    contact by email feature complete!

--- db/queries/selectEmailOfPerson.ts
+++ db/queries/selectEmailOfPerson.ts
@@ -2,10 +2,8 @@ import pool from "../pool.ts";
 import recaseKeys from "../../helperFunctions/recaseKeys.ts";
 
 async function selectEmailOfPerson(itemPrimaryKey: string){
-  const response = recaseKeys(
-    await pool.query("SELECT email_of_person_who_posted FROM items where pk=$1", [itemPrimaryKey])
-  );
-  return response.rows[0].emailOfPersonWhoPosted as string;
+  const response = await pool.query("SELECT email_of_person_who_posted FROM items where pk=$1", [itemPrimaryKey])
+  return recaseKeys(response.rows[0]).emailOfPersonWhoPosted as string;
 }
 
 export default selectEmailOfPerson;
--- handlers/contactPerson.ts
+++ handlers/contactPerson.ts
@@ -1,9 +1,32 @@
 import type { Request, Response } from "npm:express";
+import { createTransport } from "npm:nodemailer";
+import type SMTPTransport from "npm:nodemailer/lib/smtp-transport";
 import selectEmailOfPerson from "../db/queries/selectEmailOfPerson.ts";
+import 'jsr:@std/dotenv/load';
 
-const contactPerson = (req: Request, res: Response) => {
-  const email = selectEmailOfPerson(req.params.itemPrimaryKey);
+const contactPerson = async(req: Request, res: Response) => {
+  const fromEmail = Deno.env.get("EMAIL");
+  const toEmail = await selectEmailOfPerson(req.params.itemPrimaryKey);
   const { subject, message } = req.body;
+
+  const transportOptions: SMTPTransport.Options = {
+    host: Deno.env.get("SMTP_HOST"),
+    port: Number(Deno.env.get("SMTP_PORT")),
+    auth: {
+      user: fromEmail,
+      pass: Deno.env.get("APP_PASSWORD")
+    }
+  }
+
+  const transporter = createTransport(transportOptions);
+
+  await transporter.sendMail({
+    from: `"Eureka the lost and found website" <${fromEmail}>`,
+    to: toEmail,
+    subject: subject,
+    text: message
+  })
+
   res.redirect("/");
 };
 
