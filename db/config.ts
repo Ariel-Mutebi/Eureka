32bc7647acfd2cb66fddb65b06d1e22c9e8e1e4b
black-RAM <pplanes311@gmail.com>
2025-08-19 22:06:29 +0300

    configure ssl

--- db/config.ts
+++ db/config.ts
@@ -2,7 +2,8 @@ import type { PoolConfig } from "npm:pg";
 import 'jsr:@std/dotenv/load';
 
 const databaseClientConfig: PoolConfig = {
-  connectionString: Deno.env.get("PG_CONNECTION_STRING")
+  connectionString: Deno.env.get("PG_CONNECTION_STRING"),
+  ssl: { rejectUnauthorized: false }
 };
 
 export default databaseClientConfig;
