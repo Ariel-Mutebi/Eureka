d28f96668e5691b24ac5cefc2856451d6bef8c9c
black-RAM <pplanes311@gmail.com>
2025-08-19 22:49:15 +0300

    table creation in populate.sql

--- db/populate.sql
+++ db/populate.sql
@@ -1,6 +1,16 @@
+CREATE TABLE IF NOT EXISTS items (
+  pk integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name character varying(255) NOT NULL,
+  description character varying(255) NOT NULL,
+  email_of_person_who_posted character varying(255) NOT NULL,
+  password character varying(255) NOT NULL,
+  is_lost boolean NOT NULL,
+  location point NOT NULL
+);
+
 INSERT INTO items
 (name, description, email_of_person_who_posted, password, is_lost, location)
 VALUES
 ('Bike', 'This is an example.', 'eurekalostandfound@gmail.com', 'Bro really thinks he is an admin...', TRUE, point(41.8902335, 12.4911639)),
 ('Car keys', 'This is an example.', 'eurekalostandfound@gmail.com', 'Bro really thinks he is an admin...', TRUE, point(43.7230159, 10.3966322)),
-('Wedding ring', 'This is an example.', 'eurekalostandfound@gmail.com', 'Bro really thinks he is an admin...', FALSE, point(48.8582599, 2.2945006))
+('Wedding ring', 'This is an example.', 'eurekalostandfound@gmail.com', 'Bro really thinks he is an admin...', FALSE, point(48.8582599, 2.2945006));
